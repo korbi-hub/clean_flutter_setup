8c02f8f7537bd4980a975e5f672f67b0decb71e4
Korbinian Kaiser <korbinian.kaiser@intive.com>
2024-01-16 10:55:05 +0100

    removed unnecessary create_flutter.sh file

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 current_directory=$(pwd)
 
-chmod +x "$current_directory/create_flutter.sh"
+chmod +x "$current_directory/create_flutter.py"
 
-# Add the script's directory to the PATH
-export PATH="$current_directory:$PATH"
+echo "export PATH=\$PATH:$current_directory" >> ~/.zshrc
 
-echo "Setup completed! You can now run the script by typing 'create_flutter' in the command prompt."
+# python3 $(which create_flutter.py)
